a8b53d2a210e724b2e04077e0b07dd223197955d
Damian Legawiec <damian@sparksolutions.co>
2021-09-21 17:04:59 +0200

    Setup spree frontend as a seperate repo

--- bin/build-ci.rb
+++ bin/build-ci.rb
@@ -21,7 +21,7 @@ class Project
     @name = name
   end
 
-  ALL = %w[api backend core frontend emails].map(&method(:new)).freeze
+  ALL = %w[frontend].map(&method(:new)).freeze
   CORE_GEMS = %w[api core].freeze
 
   # Install subproject
---
+++ frontend/lib/spree/frontend/version.rb
@@ -0,0 +1,9 @@
+module Spree
+  module Frontend
+    VERSION = '4.4.0.alpha'.freeze
+
+    def self.version
+      VERSION
+    end
+  end
+end
