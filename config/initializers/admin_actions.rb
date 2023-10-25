b2e7f914759681ac67342a106b48bc3f48610b4c
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-10-25 18:14:36 +0200

    Append items to config instead of overwriting

--- config/initializers/admin_actions.rb
+++ config/initializers/admin_actions.rb
@@ -1,3 +1,4 @@
 Rails.application.config.after_initialize do
   Rails.application.config.spree_backend.actions[:product] = Spree::Admin::Actions::ProductPreviewActionBuilder.new.build
+  Rails.application.config.spree_backend.actions.include?(:images) ? (Rails.application.config.spree_backend.actions[:images].items << Spree::Admin::Actions::ProductPreviewActionBuilder.new.build.items).flatten! : Rails.application.config.spree_backend.actions[:images] = Spree::Admin::Actions::ProductPreviewActionBuilder.new.build
 end
