5e1a2a25c90fece778ba02beb4069cd167abeb7f
Chris Laai <134155599+chrislaai@users.noreply.github.com>
2023-12-14 22:25:55 +0800

    feat: Show vendor info

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Spree::Core::Engine.add_routes do
   scope '(:locale)', locale: /#{Spree.available_locales.join('|')}/, defaults: { locale: nil } do
     root to: 'home#index'
 
+    resources :vendors
+
     resources :products, only: [:index, :show], path: '/products'
 
     get '/products/:id/related', to: 'products#related'
