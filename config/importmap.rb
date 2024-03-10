e8fcbcebe7e457772f6a8d6784bf26323431ad47
Damian Legawiec <damian@getvendo.com>
2024-03-10 19:04:23 +0100

    Setup importmap

---
+++ config/importmap.rb
@@ -0,0 +1,2 @@
+pin "application-spree-frontend", to: "spree/frontend/application.js", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
--- lib/spree/frontend/engine.rb
+++ lib/spree/frontend/engine.rb
@@ -9,11 +9,15 @@ module Spree
       config.action_view.sanitized_allowed_tags = %w(a b del em i ins mark p small strong sub sup)
       config.action_view.sanitized_allowed_attributes = %w(href)
 
-      # sets the manifests / assets to be precompiled, even when initialize_on_precompile is false
-      initializer 'spree.assets.precompile', group: :all do |app|
-        app.config.assets.precompile += %w[
-          spree/frontend/all*
-        ]
+      initializer "spree.frontend.assets" do |app|
+        app.config.assets.paths << root.join("app/javascript")
+        app.config.assets.precompile += %w[ spree_frontend_manifest ]
+      end
+
+      initializer 'spree.frontend.importmap', before: 'importmap' do |app|
+        app.config.importmap.paths << root.join('config/importmap.rb')
+        # https://github.com/rails/importmap-rails?tab=readme-ov-file#sweeping-the-cache-in-development-and-test
+        app.config.importmap.cache_sweepers << root.join('app/javascript')
       end
 
       initializer 'spree.frontend.environment', before: :load_config_initializers do |_app|
